4254168629c2c263a9b02a9476eeafdba15287cf
Jessica Rodriguez Cabrera <jessicarodriguezcabrera@Jessicas-MacBook-Pro.local>
2022-03-18 16:37:43 -0400

    erasing FFIthreeJS, copying info on ThreeJS.purs

--- src/ThreeJS.purs
+++ src/ThreeJS.purs
@@ -8,6 +8,8 @@ import Graphics.Three.Scene as Scene
 import Graphics.Three.Camera as Camera
 import Graphics.Three.Object3D as Object3D
 
+import Data.Foreign.EasyFFI (unsafeForeignProcedure, unsafeForeignFunction)
+
 -- import Graphics.Three.Renderer as Renderer
 -- import Graphics.Three.Geometry as Geometry
 -- import Graphics.Three.Material as Material
@@ -71,3 +73,61 @@ foreign import clipAction :: AnimationMixer -> AnimationClip -> Effect Animation
 foreign import setEffectiveTimeScale :: AnimationAction -> Number -> Effect Unit
 
 foreign import play :: forall o. o -> Effect Unit
+
+-----------------------------
+-----------------------------
+-- EasyFFI
+
+-- TEXTURES / MATERIAL
+
+foreign import data TextureLoader             :: Type
+foreign import data VideoTexture              :: Type
+
+class Texture a
+instance textureTextureLoader :: Texture TextureLoader
+
+-- Importing
+
+textureLoader :: String -> Effect TextureLoader
+textureLoader = unsafeForeignFunction ["param", ""] "new THREE.TextureLoader().load(param)"
+
+createVideoById :: String -> Effect Unit
+createVideoById = unsafeForeignFunction ["id", ""] "document.createElement(id)"
+
+-- STILL IMPLEMENTING
+-- addVideoTexture :: forall opt. {|opt} -> Effect VideoTexture
+-- addVideoTexture = unsafeForeignFunction ["video", ""] "new THREE.VideoTexture(video)"
+
+-- Wrapping modes
+
+wrapSclampToEdge :: forall a. Texture a => a -> Effect Unit
+wrapSclampToEdge = unsafeForeignFunction ["texture", ""] "texture.wrapS = THREE.ClampToEdgeWrapping;"
+
+wrapTclampToEdge :: forall a. Texture a => a -> Effect Unit
+wrapTclampToEdge = unsafeForeignFunction ["texture", ""] "texture.wrapT = THREE.ClampToEdgeWrapping;"
+
+wrapSrepeat :: forall a. Texture a => a -> Effect Unit
+wrapSrepeat = unsafeForeignFunction ["texture", ""] "texture.wrapS = THREE.RepeatWrapping;"
+
+wrapTrepeat :: forall a. Texture a => a -> Effect Unit
+wrapTrepeat = unsafeForeignFunction ["texture", ""] "texture.wrapT = THREE.RepeatWrapping;"
+
+wrapSmirroredRepeat :: forall a. Texture a => a -> Effect Unit
+wrapSmirroredRepeat = unsafeForeignFunction ["texture", ""] "texture.wrapS = THREE.MirroredRepeatWrapping;"
+
+wrapTmirroredRepeat :: forall a. Texture a => a -> Effect Unit
+wrapTmirroredRepeat = unsafeForeignFunction ["texture", ""] "texture.wrapT = THREE.MirroredRepeatWrapping;"
+
+--
+
+repeatSet :: forall a. Texture a => a -> Number -> Number -> Effect Unit
+repeatSet = unsafeForeignProcedure ["texture", "x", "y", ""] "texture.repeat.set(x,y);"
+
+
+-- GENERAL
+
+requestAnimationFrame :: Effect Unit -> Effect Unit
+requestAnimationFrame = unsafeForeignProcedure ["callback", ""] "window.requestAnimationFrame(callback)"
+
+unsafePrint :: forall a. a -> Effect Unit
+unsafePrint = unsafeForeignProcedure ["a", ""] "console.log(a)"
